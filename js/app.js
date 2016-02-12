5f23b3a2b0c8ee0806901c26a5b95d31e8a23b2d
tdhale <tdhale@xactware.com>
2016-02-12 15:35:44 -0700

    cleaned up a bit

--- js/app.js
+++ js/app.js
@@ -11,23 +11,28 @@
         "ngStorage"
     ])
 
-    .config(["$stateProvider", "$urlRouterProvider",
-        function ($stateProvider, $urlRouterProvider) {
+    .config(AppConfig);
 
-            // define all app states (pages)
-            $stateProvider
-                .state("home", {
-                    url: "/home",
-                    templateUrl: "templates/home.html",
-                    controller: "homeController as hc"
-                })
-                .state("about", {
-                    url: "/about",
-                    templateUrl: "templates/about.html"
-                });
+    AppConfig.$inject = ["$stateProvider", "$urlRouterProvider", "$localStorageProvider"];
+    function AppConfig($stateProvider, $urlRouterProvider, $localStorageProvider) {
 
-            // if none of the above states are matched, use this as the fallback
-            $urlRouterProvider.otherwise("/home");
-        }]);
+        // pick a unique app prefix for local storage
+        $localStorageProvider.setKeyPrefix('tdapp');
+
+        // define all app states (pages)
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: "templates/home.html",
+                controller: "homeController as hc"
+            })
+            .state("about", {
+                url: "/about",
+                templateUrl: "templates/about.html"
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise("/home");
+    }
 
 }());
